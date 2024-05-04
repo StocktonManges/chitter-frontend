8390c6b63acb6623afa42116ad4eb55cec68e0e9
StocktonManges <stocktonmanges@gmail.com>
2024-05-04 13:06:33 -0600

    Added official backend url

--- src/utils.ts
+++ src/utils.ts
@@ -2,15 +2,7 @@ import { postCalls } from "./api/posts";
 import { userCalls } from "./api/users";
 import { Post, User } from "./types";
 
-// gcp-main-firebase serve URL
-// export const fetchURL =
-//   "http://127.0.0.1:5001/mini-social-media-1e822/us-central1/app/";
-
-// Main local URL
-export const fetchURL = "http://localhost:3000/";
-
-// Production local URL
-// export const fetchURL = "http://localhost:3001/";
+export const fetchURL = "https://sqliteapp-ttpxmmzj3a-uc.a.run.app";
 
 export const getAllUsers = (setState: (value: User[]) => void) =>
   userCalls.getUsers().then((result) => setState(result));
